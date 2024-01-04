9457072245132aadd4e9568422d94a0f4fada308
abhishekks2404 <abhishek.singh@blenheimchalcot.com>
2024-01-04 17:12:36 +0530

    final2

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ model_id = 'TheBloke/Mistral-7B-Instruct-v0.1-GGUF'
 # model_id = 'TheBloke/Mistral-7B-codealpaca-lora-GGUF'
 os.environ['XDG_CACHE_HOME']= './model'
 config  ={'temperature':1.00,'context_length':5000,}
-#shjfgshjgfhsgfhsgfhsgjdgfjhsdggfds
 llm = CTransformers(model = model_id, model_type='mistral', config=config, callbacks= [StreamingStdOutCallbackHandler()])
 
 print(llm("Write a poem on guitar."))
