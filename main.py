470b07f4c052cd342093b6abd98dee6917018087
abhishekks2404 <abhishek.singh@blenheimchalcot.com>
2024-01-04 16:28:52 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from langchain.llms import CTransformers 
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferMemory
+
+model_id = 'TheBloke/Mistral-7B-Instruct-v0.1-GGUF'
+# model_id = 'TheBloke/Mistral-7B-codealpaca-lora-GGUF'
+os.environ['XDG_CACHE_HOME']= './model'
+config  ={'temperature':1.00,'context_length':5000,}
+llm = CTransformers(model = model_id, model_type='mistral', config=config, callbacks= [StreamingStdOutCallbackHandler()])
+
+print(llm("Write a poem on guitar."))
